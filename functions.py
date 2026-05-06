a9f02a7c6e32a6521ac2a32fa052c33e2cd869f1
Bilal <bilalkhamed1@gmail.com>
2026-05-06 21:26:19 +0300

    Enhance game filtering and adding functionality with improved error handling and validation

--- functions.py
+++ functions.py
@@ -22,32 +22,47 @@ def find_game(query, data):
 def min_metacritic(minimum,data):
     counter = 0
     for game in data.keys():
-        if int(data[game].metacritic) >= minimum:
+
+        score = None
+        try: 
+            score = int(data[game].metacritic)
+        except:
+            continue
+            
+        if score >= minimum:
             console.print(f"\n🎮 [bold magenta]{game}[/]")
-            console.print(f"  [cyan]Metacritic:[/]   {data[game].metacritic}")
+            console.print(f"  [cyan]Metacritic:[/]   {score}")
             counter += 1
     if counter == 0:
-        console.print("No game with higher or equal metacritic score. Please try again.", style="red")
+        console.print("No game with higher or equal metacritic score. Please try with a different filter.", style="red")
     else:
         console.print(f'\n✅ Found [bold green]{counter}[/] game(s)')
 
 def max_price(maximum,data):
     counter = 0
     for game in data.keys():
-        if int(data[game].price)<=maximum:
+
+        # if a price does not exist (None), then skip
+        price = None
+        try:
+            price = float(data[game].price)
+        except:
+            continue
+    
+        if price<=maximum:
             console.print(f"\n🎮 [bold magenta]{game}[/]")
-            console.print(f"  [cyan]Price:[/]        ${data[game].price}")
+            console.print(f"  [cyan]Price:[/]        ${price}")
             counter += 1
     if counter == 0:
-        console.print("No game with lower or equal price. Please try again.", style="red")
+        console.print("No game with lower or equal price. Please try with a different filter.", style="red")
     else:
         console.print(f'\n✅ Found [bold green]{counter}[/] game(s)')
        
 def recommend_game(game_name, data):
     dic = {}
     for game in data:
-        intersect = set(data[game_name].tags) & set(data[game].tags)
-        union = set(data[game_name].tags) | set(data[game].tags)
+        intersect = set([tag.lower() for tag in data[game_name].tags]) & set([tag.lower() for tag in data[game].tags])
+        union = set([tag.lower() for tag in data[game_name].tags]) | set([tag.lower() for tag in data[game].tags])
         jaccard = len(intersect) / len(union)
         dic[game] = jaccard
     lst = sorted([(v, k) for (k, v) in dic.items()], reverse=True)
@@ -62,19 +77,49 @@ def add_game():
 
     name = questionary.text("Game name:").ask()
     developer = questionary.text("Developer (or None if it does not exist):").ask()
-    release_year = int(questionary.text("Release year (or None if it does not exist):").ask())
-    metacritic = float(questionary.text("Metacritic score (or None if it does not exist):").ask())
-    price = float(questionary.text("Price (or None if it does not exist):").ask())
+    release_year = questionary.text("Release year (or None if it does not exist):").ask()
+
+    # here we are validating each value to ensure its a number, otherwise default to None
+    try:
+        if release_year and release_year != 'None':
+            release_year = int(release_year)
+        else:
+            release_year = None
+    except ValueError:
+        console.print('Release year must be a number. Defaulting to None.', style='red')
+        release_year = None
+        
+    metacritic = questionary.text("Metacritic score (or None if it does not exist):").ask()
+    try:
+        if metacritic and metacritic != 'None':
+            metacritic = float(metacritic)
+        else:
+            metacritic = None
+    except ValueError:
+        console.print('Metacritic score must be a number. Defaulting to None.', style='red')
+        metacritic = None
+
+    price = questionary.text("Price (or None if it does not exist):").ask()
+    try:
+        if price and price != 'None':
+            price = float(price)
+        else:
+            price = None
+    except ValueError:
+        console.print('Price must be a number. Defaulting to None.', style='red')
+        price = None
+
     tags = questionary.text("Tags separated by commas (or None if it does not exist):").ask()
 
-    if tags:
-        tags = [tag.strip() for tag in tags.split(",")]
+    # if there are tags, store them in a list (split by commas)
+    if tags and tags != 'None':
+        tags = [tag.strip() for tag in tags.strip().split(",")]
     else:
         tags = None
     
     new_game = Game(name, tags, release_year, metacritic, price, developer)
-    new_game.save('games_database.json')
     try:
+        new_game.save('games_database.json')
         console.print('Game saved successfuly', style='green')
     except:
         console.print('An error occurred', style='red')
